a84e56955a4199a26c7494629183bc5ff1edc133
heliofeliciano <heliofeliciano@gmail.com>
2023-01-17 05:03:36 -0300

    Initital commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Helio Feliciano on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
