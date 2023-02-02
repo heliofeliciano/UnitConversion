4e9092ce44474a1ee39155161774f1745b7c816e
heliofeliciano <heliofeliciano@gmail.com>
2023-02-02 06:09:53 -0300

    converting from meter to feet

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -1,21 +1,71 @@
-//
-//  ContentView.swift
-//  UnitConversion
-//
-//  Created by Helio Feliciano on 17/01/23.
-//
-
 import SwiftUI
 
+enum TemperatureConversion {
+    case Celsius, Fahrenheit, Kelvin
+}
+
+enum LengthConversion: String, Equatable, CaseIterable {
+    case meters = "M"
+    case kilometers = "KM"
+    case feet = "FT"
+    case yards = "YD"
+    case miles = "MI"
+}
+
+enum TimeConversion {
+    case seconds, minutes, hours, days
+}
+
+enum VolumeConversion {
+    case milliliters, liters, cups, pints, gallons
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var inputSelection: LengthConversion = .meters
+    @State var outputSelection: LengthConversion = .kilometers
+    @State var numberToConvert: Double = 0.0
+    var result: Double {
+        switch (inputSelection, outputSelection) {
+        case (.meters, .feet):
+            let numberInMeters = Measurement(value: numberToConvert, unit: UnitLength.meters)
+            return numberInMeters.converted(to: UnitLength.feet).value
+        default:
+            return numberToConvert
         }
-        .padding()
+    }
+    var body: some View {
+        Form {
+            Section {
+                Picker("LenghtConversion", selection: $inputSelection) {
+                    ForEach(LengthConversion.allCases, id: \.self) { value in
+                        Text(value.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Input Length Conversion")
+            }
+            Section {
+                Picker("LenghtConversion", selection: $outputSelection) {
+                    ForEach(LengthConversion.allCases, id: \.self) { value in
+                        Text(value.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Output Length Conversion")
+            }
+            Section {
+                TextField("Number to convert",
+                          value: $numberToConvert,
+                          format: .number)
+            } header: {
+                Text("What number do you want to convert?")
+            }
+            Section {
+                Text(result, format: .number)
+            }
+        }
     }
 }
 
